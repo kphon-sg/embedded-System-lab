ff19c62e2f2124f88e5d5167f0b6342873184ea4
raikyyy <chukongphongg@gmail.com>
2025-05-12 21:48:51 +0700

    n

--- adc-glcd-led-joystick-uart/GLCD.c
+++ adc-glcd-led-joystick-uart/GLCD.c
@@ -1,7 +1,18 @@
 #include "GLCD.h"
 
-void GLCD_Initialize(void) {}
+    
+void GLCD_Initialize(void) {
+    GLCD_Initialize();              
+    GLCD_SetBackgroundColor(Black);
+    GLCD_SetForegroundColor(White);
+    GLCD_ClearScreen();
+}
 
-void GLCD_Clear(void) {}
 
-void GLCD_DisplayString(int x, int y, char* str) {}
+void GLCD_Clear(void) {
+    GLCD_ClearScreen();
+}
+
+void GLCD_DisplayString(int x, int y, char* str) {
+    GLCD_DisplayString(x, y, 1, (unsigned char *)str); 
+}
